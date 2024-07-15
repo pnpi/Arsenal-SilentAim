19bd21c0cfd64ceb05bdd4d78263488f59270908
pnpi <fargosog@gmail.com>
2024-07-15 18:28:05 -0400

    Print default arguments for workspace methods.

--- Internal.lua
+++ Internal.lua
@@ -1,11 +1,21 @@
 local ogNamecall = nil 
 ogNamecall = hookmetamethod(game, "__namecall", newcclosure(function(Self, ...)
-      local Args = {...}
-      local Method = getnamecallmethod()
-      
-      if Self == game.Workspace then
-        print("Methods called by workspace: ", Method) 
-      end
-      
-      return ogNamecall(Self, ...)
+    local Args = {...}
+    local Method = getnamecallmethod()
+    
+    if Self == game.Workspace and not checkcaller() then
+        if Method == "Raycast" then
+            local Origin = Args[1]
+            local Direction = Args[2]
+            print("Raycast called with Origin:", Origin, "Direction:", Direction)
+            
+        elseif Method == "FindPartOnRayWithWhitelist" then
+            local Ray = Args[1]
+            local Whitelist = Args[2]
+            local IgnoreWater = Args[3] or false
+            print("FindPartOnRayWithWhitelist called with Ray:", Ray, "Whitelist:", Whitelist, "IgnoreWater:", IgnoreWater)
+        end
+    end
+    
+    return ogNamecall(Self, ...)
 end))
